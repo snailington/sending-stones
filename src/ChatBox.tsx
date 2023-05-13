646b79fdad3ef5c611b3a16e12550651f6e8fb94
snail <homeless@snail.moe>
2023-05-13 15:09:55 -0400

    refactored client API

--- src/ChatBox.tsx
+++ src/ChatBox.tsx
@@ -2,13 +2,13 @@ import {useEffect, useState} from "react";
 import ChatBubble from "./ChatBubble.tsx";
 
 import "./ChatBox.css";
-import {onMessage} from "./client.ts";
+import MagicCircle from "./client.ts";
 import {MsgRPC} from "./RPC.ts";
 
 function ChatBox() {
     const [messageLog, setMessageLog] = useState(new Array<MsgRPC>());
     
-    useEffect(() => onMessage(messageLog[0]?.time, (msg) => {
+    useEffect(() => MagicCircle.onMessage(messageLog[0]?.time, (msg) => {
         setMessageLog([msg, ...messageLog]);
     }), [messageLog]);
     
