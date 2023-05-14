d8344e8c8616ad0d7170ce2e5fe0435704c189af
snail <homeless@snail.moe>
2023-05-14 01:21:36 -0400

    forgot to add the buttonbar files

---
+++ src/ButtonBar.tsx
@@ -0,0 +1,13 @@
+import "./ButtonBar.css"
+
+function ButtonBar() {
+    return (
+        <div className="button-bar">
+            <button></button>
+            <button></button>
+            <button></button>
+        </div>
+    );
+}
+
+export default ButtonBar;
