4b39b39d99c4f8b175397bfb3934137524f06c38
snail <homeless@snail.moe>
2023-05-17 14:23:53 -0400

    updated dice message formatting

--- src/Bubbles/DiceBubble.tsx
+++ src/Bubbles/DiceBubble.tsx
@@ -1,6 +1,16 @@
-import {Message} from "magic-circle-api";
+import {Message, RollInfo} from "magic-circle-api";
 
 export default function DiceBubble({message}: {message: Message}) {
+    const rollInfo = message.metadata as RollInfo;
+
+    let diceString = "";
+    for(const diceType of new Set(rollInfo.dice)) {
+        const count = rollInfo.dice.reduce((acc: number, d) => d == diceType ? acc + 1 : 0, 0);
+        diceString += `${count}d${diceType} `;
+    }
+    diceString = diceString.trimEnd() + (rollInfo.modifier == 0 ? "" :
+        (rollInfo.modifier > 0 ? `+` : "") + rollInfo.modifier);
+
     return (
         <div className="chat-bubble">
             <div className="msg-header">
@@ -9,6 +19,10 @@ export default function DiceBubble({message}: {message: Message}) {
             <div className="msg-body">
                 <div className="msg-total">{message.metadata.total}</div>
                 <div className="msg-text">{message.text}</div>
+            </div>
+            <div className="msg-footer">
+                {diceString}
+                {rollInfo.results ? ` = [${rollInfo.results.join(', ')}]`: ""}
             </div>
         </div>
     );
