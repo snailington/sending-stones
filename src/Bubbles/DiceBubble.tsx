cddc836f4e18bb640774ccf86cb12f9939f00677
snail <homeless@snail.moe>
2023-05-22 16:02:18 -0400

    made style message totals a bit larger

--- src/Bubbles/DiceBubble.tsx
+++ src/Bubbles/DiceBubble.tsx
@@ -1,13 +1,15 @@
 import MagicCircle, {DiceMessage} from "magic-circle-api";
+import "./DiceBubble.css";
 
 export default function DiceBubble({message}: {message: DiceMessage}) {
     return (
         <div className="chat-bubble">
+            <div className="msg-total">{message.metadata.total}</div>
             <div className="msg-header">
                 <div className="msg-author">{message.author}</div>
             </div>
             <div className="msg-body">
-                <div className="msg-total">{message.metadata.total}</div>
+
                 <div className="msg-text">{message.text}</div>
             </div>
             <div className="msg-footer">
