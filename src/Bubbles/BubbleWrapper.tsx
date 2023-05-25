99419c1085d5f5f0b1030bf4611e08f9fb4f7560
snail <homeless@snail.moe>
2023-05-25 03:31:23 -0400

    updated message bubble coloring

--- src/Bubbles/BubbleWrapper.tsx
+++ src/Bubbles/BubbleWrapper.tsx
@@ -3,11 +3,17 @@ import {Message} from "magic-circle-api";
 import OBR from "@owlbear-rodeo/sdk";
 
 export default function BubbleWrapper({children, message}: {children: ReactNode, message: Message}) {
-    const [background, setBackground] = useState("");
+    const [background, setBackground] = useState(undefined as string|undefined);
     
     async function makeGradient() {
         let color: string;
-        if(message.player == OBR.player.id) {
+        const theme = await OBR.theme.getTheme();
+        const endColor = theme.mode == "DARK" ? "#00000020" : "#FFFFFF1A";
+
+        if(!message.player) {
+            setBackground(endColor);
+            return;
+        } else if(message.player == OBR.player.id) {
             color = await OBR.player.getColor();
         } else {
             const players = await OBR.party.getPlayers();
@@ -16,13 +22,19 @@ export default function BubbleWrapper({children, message}: {children: ReactNode,
             color = found.color;
         }
         
-        setBackground(`linear-gradient(${color}F0 1px, ${color}2A 3px, transparent 24px`);
+        setBackground(`linear-gradient(${color}F0 1px, ${color}2A 3px, ${endColor} 24px`);
     }
+
+    useEffect(() => OBR.theme.onChange(() => {
+        makeGradient();
+    }), [])
     
     useEffect(() => {
         makeGradient();
     }, []);
     
+    if(background == undefined) return <></>;
+
     const style: CSSProperties = {};
     style.background = background;
     
