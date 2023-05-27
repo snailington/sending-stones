5b2d94aa239aebe634e1841a74b88fc92b5af27a
snail <homeless@snail.moe>
2023-05-27 05:38:05 -0400

    added unread message badge

--- src/MessageBox.tsx
+++ src/MessageBox.tsx
@@ -1,17 +1,29 @@
 import {useEffect, useState} from "react";
 import MessageBubble from "./MessageBubble.tsx";
-
-import "./MessageBox.css";
-
 import MagicCircle, {Message} from "magic-circle-api"
+import OBR from "@owlbear-rodeo/sdk";
+import "./MessageBox.css";
 
 function MessageBox() {
     const [messageLog, setMessageLog] = useState(new Array<Message>());
-    
-    useEffect(() => MagicCircle.onMessage(messageLog[0], (msgs) => {
+    const [unread, setUnread] = useState(-1);
+
+    useEffect(() => MagicCircle.onMessage(messageLog[0], async (msgs) => {
         setMessageLog([...(msgs.reverse()), ...messageLog]);
-    }), [messageLog]);
-    
+        if(!await OBR.action.isOpen()) {
+            const nowUnread = unread + 1;
+            if(nowUnread > 0) {
+                OBR.action.setBadgeText(nowUnread.toString());
+            }
+            setUnread(nowUnread);
+        }
+    }), [unread, messageLog]);
+
+    useEffect(() => OBR.action.onOpenChange((isOpen) => {
+        setUnread(0);
+        if(isOpen) OBR.action.setBadgeText(undefined);
+    }), []);
+
     return (
         <div className="chat-box">
             {messageLog.map((m) => <MessageBubble key={m.id} message={m} />)}
